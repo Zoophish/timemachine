e655d74a8eeea57c166e2c62c258c3cd0fee7baf
Zoophish <Samuel.Warren0@gmail.com>
2025-03-27 16:39:24 +0000

    pre-optimisation changes

--- timemachine/models/feature_layer/differentiable_feature_layer.py
+++ timemachine/models/feature_layer/differentiable_feature_layer.py
@@ -20,7 +20,8 @@ class DifferentiableFeatureLayer(nn.Module):
         self,
         feature_configs: List[Dict],
         input_dim: int,
-        max_window : int = 128,
+        device : torch.device,
+        max_lookback_size : int = 128,
         pad_mode : str = 'zeros',
     ):
         """
@@ -35,7 +36,7 @@ class DifferentiableFeatureLayer(nn.Module):
                 - 'fixed_params': Dict of fixed parameter values (optional)
                 - 'out_size': int, optional. The output dimensions of the feature. 1 if unspecified.
             input_dim: Number of input features in the time series
-            max_window: A limit on how far back rolling features can look, mainly for performance.
+            max_lookback_size: A limit on how many previous time steps the feature funcs can get per batch.
             pad_mode: How to handle padding for lookback windows ('zeros' or 'replicate')
         """
         super().__init__()
@@ -43,12 +44,13 @@ class DifferentiableFeatureLayer(nn.Module):
         self.feature_configs = feature_configs
         self.input_dim = input_dim
         self.pad_mode = pad_mode
+        self.device = device
 
         self.batch_norms = nn.ModuleList()
         for _ in range(len(feature_configs)):
             self.batch_norms.append(nn.BatchNorm1d(input_dim))
 
-        self.max_window = max_window
+        self.max_lookback_size = max_lookback_size
 
         # determine total feature output dimensions
         self.feature_output_dims = sum([config.get('out_size', 1) for config in self.feature_configs])
@@ -90,7 +92,7 @@ class DifferentiableFeatureLayer(nn.Module):
             # store fixed parameters
             for param_name, value in config.get('fixed_params', {}).items():
                 param_key = f"{feature_name}_{param_name}"
-                self.fixed_params[param_key] = value
+                self.fixed_params[param_key] = torch.full((self.input_dim, ), value)
     
     def _get_param(self, feature_name: str, param_name: str) -> Union[torch.Tensor, float, int]:
         """Get a parameter (learnable or fixed) for a feature."""
@@ -105,13 +107,6 @@ class DifferentiableFeatureLayer(nn.Module):
 
             # scale to get a value between 1 and max_window
             return min_val + torch.sigmoid(raw_param) * (max_val - min_val)
-            
-            # # constrain alpha parameters to be between 0 and 1
-            # if param_name in ['alpha', 'decay', 'threshold', 'window_size']:
-            #     return torch.sigmoid(raw_param)
-            
-            # default: return the raw parameter
-            return raw_param
         
         elif param_key in self.fixed_params:
             return self.fixed_params[param_key]
@@ -147,6 +142,51 @@ class DifferentiableFeatureLayer(nn.Module):
                         print(f"  {name} no gradient!")
     
 
+    def _get_batch_lookback(
+            self,
+            indices : torch.Tensor,
+            full_series : torch.Tensor
+        ) -> torch.Tensor:
+        """
+        For each local window in the batch, fetches the max_lookback_size elements from the full series
+        that precede that window. This is so feature operations can access elements further back
+        than the window, but without loading the entire full series every time.
+
+        Applies padding if the lookback goes beyond the beginning of full series.
+
+        Args:
+            indices: The start and end indices in the full series for each batch item. (batch_size, 2)
+            full_series: The entire time series data. (length, input_dim)
+        Returns:
+            torch.Tensor of shape (batch_size, max_lookback_size, input_dim).
+        """
+        batch_size, _ = indices.shape
+        _, input_dim = full_series.shape
+        seq_len = indices[0, 1].item() - indices[0, 0].item()
+
+        lookback_batch = torch.zeros(  # zeros padding for now
+            (batch_size, self.max_lookback_size + seq_len, input_dim),
+            device=self.device
+        )
+        
+        for b in range(batch_size):
+            lookback = self.max_lookback_size
+            # start/end index for this batch item's data in full_series
+            seq_start_idx = indices[b, 0].item()
+            seq_end_idx = indices[b, 1].item()
+            
+            # get the global start index of the lookback window for this batch
+            window_start_idx = max(0, seq_start_idx - lookback)
+
+            # account for padding (lookback window goes past the start of full_series, where there's no data - leave as 'zeros')
+            # we could have pad the data retrieved from full_series below, but this should me marginally faster
+            pad_size = abs(min(seq_start_idx - lookback, 0))
+
+            # the sequence goes back enough for the window start for the first element and the end of the input sequence
+            lookback_batch[b, pad_size:, :] = full_series[window_start_idx:seq_end_idx]
+        return lookback_batch
+    
+
     def _apply_feature(
         self,
         feature_config: Dict,
@@ -170,73 +210,55 @@ class DifferentiableFeatureLayer(nn.Module):
 
         feature_name = feature_config['name']
         feature_func = feature_config['function']
+        feature_type = feature_config['type']
         feature_output_size = feature_config.get('out_size', 1) * self.input_dim
+        batch_size, window_size, input_dim = x.shape
+        device = x.device
         
         # get the learnable parameters
         params = {
             param_name: self._get_param(feature_name, param_name)
             for param_name in feature_config.get('learnable_params', {}).keys()
         }
-        
         # get the fixed parameters
         for param_name in feature_config.get('fixed_params', {}).keys():
             params[param_name] = self._get_param(feature_name, param_name)
         
-        batch_size, seq_len, input_dim = x.shape
-        device = x.device
-        
         # get start and end indices for each batch item
         global_start_indices = indices[:, 0]
         global_end_indices = indices[:, 1]
-        
-        # sanity check
-        assert torch.all(global_end_indices - global_start_indices == seq_len), "Sequence length mismatch"
+        assert torch.all(global_end_indices - global_start_indices == window_size), "Window size must be the same across the batch"
         
         # output tensor (batch_size, seq_len, feature_output_size)
-        result = torch.zeros((batch_size, seq_len, feature_output_size), device=device, dtype=torch.float32)
-        # result = torch.zeros_like(x)
-        
-        # window size is a special case because it needs access to the full series
-        # NOTE there may be a more elegant way to do this...
-        if 'window_size' in params:
-            # get the lookback windows for each item in the batch
-            batch_windows = torch.zeros((batch_size, self.max_window + seq_len, input_dim), device=device)  # zeros padding for now
-            for b in range(batch_size):
-                lookback = self.max_window
-                # start/end index for this batch item's data in full_series
-                seq_start_idx = global_start_indices[b].item()
-                seq_end_idx = global_end_indices[b].item()
-                
-                # get the global start index of the lookback window for this batch
-                window_start_idx = max(0, seq_start_idx - lookback)
+        result = torch.zeros((batch_size, window_size, feature_output_size), device=device)
 
-                # account for padding (lookback window goes past the start of full_series, where there's no data - leave as 'zeros')
-                pad_size = abs(min(seq_start_idx - lookback, 0))
-                # we could have pad the data retrieved from full_series below, but this should me marginally faster
-
-                # the sequence goes back enough for the window start for the first element and the end of the input sequence
-                batch_windows[b, pad_size:, :] = full_series[window_start_idx:seq_end_idx]
+        if feature_type == 'rolling':
+            # precompute extra lookback for the batch
+            lookback_batch = self._get_batch_lookback(
+                indices=indices,
+                full_series=full_series
+            )
 
             # batch_windows contains max_window size, but we need to 'slide' a view over this for each element in the sequence
             # Notice that we slide the start of the window here with t, but because each batch item may have a different window size,
             # the end of the window needs to be handled in the feature function
-            for t in range(seq_len):  # apply the feature func for each position in the input sequence
-                result[:, t, :] = feature_func(batch_windows[:, t:, :], max_window=lookback, **params)
-        else:
-            batch_windows = torch.zeros((batch_size, 1 + seq_len, input_dim), device=device)  # zeros padding for now
-            for b in range(batch_size):
-                seq_start_idx = global_start_indices[b].item()
-                seq_end_idx = global_end_indices[b].item()
-                lookback = 1
-                window_start_idx = max(0, seq_start_idx - lookback)
-                pad_size = abs(min(seq_start_idx - lookback, 0))
-                batch_windows[b, pad_size:, :] = full_series[window_start_idx:seq_end_idx]
+            for t in range(window_size):  # apply the feature func for each position in the input sequence
+                # the centre is counterintuitively the front of the window (formally because it's symmetric)
+                centre = self.max_lookback_size + t
+                result[:, t, :] = feature_func(lookback_batch[:, t:, :], max_window=self.max_lookback_size, **params)
+
+        elif feature_type == 'recurrent':
+            batch_windows = self._get_batch_lookback(
+                indices=indices,
+                full_series=full_series
+            )
 
             # feature doesn't use a window - use the input tensor x
             # (confusion note) instead of sliding the back of the window, we slide the front
-            for t in range(1, seq_len+1): # this is very slow, should consider changing it
-                result[:, t, :] = feature_func(batch_windows[:, 0:t, :], **params)
-        
+            context = torch.zeros((batch_size, 1, input_dim), device=device)
+            for t in range(0, window_size): # this is very slow, should consider changing it
+                result[:, t, :] = feature_func(batch_windows[:, 0:t+1, :], context, **params)
+
         return result
     
 
@@ -279,3 +301,38 @@ class DifferentiableFeatureLayer(nn.Module):
             output[:, :, output_start_idx:output_end_idx] = feature_output
         
         return output
+
+
+def indices_to_sample(
+        indices : torch.Tensor,
+        full_series: torch.Tensor,
+        scaler = None
+    ) -> torch.tensor:
+    """
+    Converts the batched indices to a batch of tensors from the full series.
+
+    Args:
+        indices: Tensor of integer indices of shape (batch_size, 2)
+        full_series: The full time series data (total_len, input_dim)
+        scaler: Input scaler, optional.
+    Returns:
+        
+    """
+
+    batch_size, _ = indices.shape
+    seq_len = indices[0, 1].item() - indices[0, 0].item() # all batch items should be the same length
+    series_len, num_channels = full_series.shape
+
+    out = torch.zeros((batch_size, seq_len, num_channels), dtype=torch.float32)
+    for b in range(batch_size):
+        index0 = indices[b, 0].item()
+        index1 = indices[b, 1].item()
+        if scaler is not None:
+            out[b, :, :] = torch.tensor(
+                scaler.transform(full_series[index0 : index1, :]),
+                dtype=torch.float32,
+            )
+        else:
+            out[b, :, :] = full_series[index0 : index1, :]
+    
+    return out
